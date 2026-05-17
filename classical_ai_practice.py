01f38585577072252fc2828bbecc861398e88bb4
A Gal <220311786+chashka-1@users.noreply.github.com>
2026-05-18 00:04:45 +0200

    began crf

--- classical_ai_practice.py
+++ classical_ai_practice.py
@@ -3,14 +3,21 @@ from urllib.parse import urlparse # for feature extraction
 from sklearn.naive_bayes import BernoulliNB # naiiiive bayes model
 from sklearn.model_selection import train_test_split # separates dataset into training data and testing data
 from sklearn.metrics import classification_report # creates a report on how well the model has done
+
+# for the crf training
+import sklearn_crfsuite
+from sklearn_crfsuite import metrics
+
 import pandas as pd # interprets csv file data
 import pickle # serializing objects (turning data types into formats they can be stored in e.g. bytes)
+
+from training_data import crf_training_data # imports the training data for the crf (too long so it looked ugly here)
 
 Domain_Keywords = ["manga", "manhwa", "manhua", "comic", "scan", "comics", "scans"]
 Category_keywords = ["manga", "manhwa", "manhua", "comics", "read", "series"]
 Chapter_Keywords = ["chapter", "chap", "ch", "ep", "episode"]
 
-def feature_extraction(url: str):
+def bayes_feature_extraction(url: str):
     url = url.lower()
 
     parsed_url = urlparse(url)
@@ -45,7 +52,7 @@ def train_naive_bayes(url_csv: str):
     urls = df["url"].tolist()
     labels = df["label"].tolist()
     
-    X =  [feature_extraction(url) for url in urls]
+    X =  [bayes_feature_extraction(url) for url in urls]
     y = labels
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -58,8 +65,65 @@ def train_naive_bayes(url_csv: str):
     with open("manga_classifier.pkl", "wb") as f:
         pickle.dump(model, f)
 
-# with open("manga_classifier.pkl", "rb") as f:  # "rb" = read binary
-#    model = pickle.load(f)
+def looks_like_title_part(token):
+    return (
+        token.isalpha() and
+        token not in Category_keywords and
+        token not in Chapter_Keywords
+    )
 
-training_csv = "training_urls.csv"
-train_naive_bayes(training_csv)
+def token_features(sequence, index: int):
+    token = sequence[index]
+
+    features = {
+        "token": token,
+
+        "is_digit": token.isdigit(),
+        "is_alpha": token.isalpha(),
+        "is_short_number": token.isdigit() and int(token) < 1000,
+
+        "is_structural_word": token in ["manga", "manhwa", "manhua", "read", "series", "comics", "title"],
+        "is_chapter_indicator": token in Chapter_Keywords,
+
+        "prev_token": sequence[index -1] if index > 0 else "START",
+        "next_token": sequence[index +1] if index < len(sequence) + 1 else "END",
+
+        "prev_title_candidate": looks_like_title_part(sequence[index-1]) if index > 0 else False,
+        "next_title_candidate": looks_like_title_part(sequence[index+1]) if index < len(sequence)-1 else False,
+
+        "prev_is_structural": sequence[index-1] in Category_keywords if index > 0 else False,
+        "prev_is_chapter_indicator": sequence[index - 1] in Chapter_Keywords if index > 0 else False,
+
+        "next_chapter_indicator": any(t in Chapter_Keywords for t in sequence[index:]),
+
+        "distance_to_chapter": next((i for i, t in enumerate(sequence[index:]) if t in Chapter_Keywords), -1),
+
+        "position": index,
+        "is_last": index == len(sequence) - 1,
+        "is_first": index == 0,
+    }
+
+    return features
+
+def sequence_feature_extraction(sequence):
+    # cycles labels every token in sequence
+    return [token_features(sequence, i) for i in range(len(sequence))]
+
+
+
+# used to actually run and test the AI model on individual instances of URLs
+def isManga():
+    url = input("Enter URL to test here: ")
+    with open("manga_classifier.pkl", "rb") as f:  # "rb" = read binary
+        model = pickle.load(f)
+
+    features = bayes_feature_extraction(url)
+    prediction = model.predict([features])
+
+    if prediction:
+        print(f'{url} IS a manga website! It will automatically be added to the database.')
+    else:
+        print(f'{url} is NOT a manga website. It will not be automatically added to the database.')
+
+if True:
+    isManga()
